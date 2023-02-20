a15f819f5e3f8b502ee1d45602895169d0e1e579
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-02-21 00:55:24 +0530

    Added: welcome to cab invoice generator

---
+++ src/main/java/com/bridgelabz/jdbc1/Day30_Cab_Invoice/App.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.jdbc1.Day30_Cab_Invoice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
